e237f30bcce2e7be6793d38493f7f2e50f58dfdf
Yuval Lifshitz <ylifshit@redhat.com>
2021-10-14 12:53:24 +0300

    AWS_DEFAULT_REGION export deleted by mistake
    needed to run the image uploads

--- scripts/s3-env.sh
+++ scripts/s3-env.sh
@@ -1,4 +1,4 @@
 USER=$(kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- radosgw-admin user list |grep ceph-user |cut -d '"' -f2)
 export AWS_ACCESS_KEY_ID=$(kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- radosgw-admin user info --uid  $USER |grep access_key|awk '{print $2}' |cut -d '"' -f2)
 export AWS_SECRET_ACCESS_KEY=$(kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- radosgw-admin user info --uid  $USER |grep secret_key|awk '{print $2}' |cut -d '"' -f2)
-
+export AWS_DEFAULT_REGION=my-store
