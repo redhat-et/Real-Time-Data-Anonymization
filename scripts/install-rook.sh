01ec54bd6912fe6538cf9badef04c84d0f51b01d
Huamin Chen <hchen@redhat.com>
2021-11-08 09:00:07 -0500

    update

--- scripts/install-rook.sh
+++ scripts/install-rook.sh
@@ -1,6 +1,6 @@
 kubectl apply -f https://raw.githubusercontent.com/rootfs/rook/microshift-int/cluster/examples/kubernetes/ceph/crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/rootfs/rook/microshift-int/cluster/examples/kubernetes/ceph/common.yaml
-kubectl apply -f https://raw.githubusercontent.com/rootfs/rook/microshift-int/cluster/examples/kubernetes/ceph/operator.yaml
+kubectl apply -f https://raw.githubusercontent.com/rootfs/rook/microshift-int/cluster/examples/kubernetes/ceph/operator-openshift.yaml
 sleep 5
 kubectl apply -f https://raw.githubusercontent.com/rootfs/rook/microshift-int/cluster/examples/kubernetes/ceph/cluster-test.yaml
 sleep 30
